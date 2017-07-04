3cab6483d5b2c477bb3d577c23807d4e19bf7a41
Per Kristian Warvik <fmsfpkw@fylkesmannen.no>
2017-07-04 16:33:58 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+(function(window, undefined) {
+  "use strict";
+
+  var editorDOM = document.querySelector("#ComponentEditor");
+  var EditorInstance = new ComponentEditor(editorDOM);
+
+})(window);
